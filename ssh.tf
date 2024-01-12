5be20e5231dd6c6269fc9647adfb49f8c0e73589
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-12 13:15:06 -0800

    Update ssh.tf

--- ssh.tf
+++ ssh.tf
@@ -1,25 +1,21 @@
 resource "aws_security_group" "my_sg" {
-       name        = "my_sg"
-       description = "allow ssh access"
-<<<<<<< HEAD
-       vpc_id      = "blueteam"
-=======
-       vpc_id      = "vpc-03e79092478532666"
->>>>>>> 3a0637e (updated ssh.tf vpc id)
+  name        = "my_sg"
+  description = "allow ssh access"
+  vpc_id      = "vpc-03e79092478532666"  # Replace with the correct VPC ID
 
-       #Not redundant - Inbound Access
-       ingress {
-         from_port   = "22"
-         to_port     = "22"
-         protocol    = "TCP"
-         cidr_blocks = ["73.116.216.200/32","76.102.161.122/32","98.45.139.47/32"]
-       }
+  # Inbound Access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["73.116.216.200/32", "76.102.161.122/32", "98.45.139.47/32"]
+  }
 
-       # Outbound Access    
-       egress {
-         from_port   = 0
-         to_port     = 0
-         protocol    = "-1"
-         cidr_blocks = ["0.0.0.0/0"]
-       }
+  # Outbound Access    
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
