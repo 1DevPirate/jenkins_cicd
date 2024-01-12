52ed921e22f508700d680144bd379b0b3c22656c
choncey <nchoncey@gmail.com>
2024-01-12 13:54:17 -0800

    updated script for apache, and others

--- apache2-bs.tf
+++ apache2-bs.tf
@@ -1,25 +1,12 @@
-#!/bin/bash
+# run for updated apps
+sudo apt update
 
-while true; do
-  # Ask the user a question
-  echo "Do you want to go to jail, or do you want to go home?"
-  read response
+# Install apache2
+sudo apt install apache2
 
-  if [ "$response" = 'home' ] || [ "$response" = 'yes' ]; then
-    echo "Glad you want to go home."
-    echo "Aight, I got you."
+# Install mysql
+sudo apt install mysql-server
 
-    # Install apache2
-    sudo apt install apache2
+# Install postgres
+sudo apt install postgresql
 
-    # Install mysql
-    sudo apt install mysql-server
-
-    # Install postgres
-    sudo apt install postgresql
-
-    break  # Exit the loop if the user makes a valid choice
-  else
-    echo "Invalid response. Please enter 'home' or 'yes'."
-  
-fi
