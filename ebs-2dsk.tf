83b2b8c5770d0f32dcdbf911c21812d68c699fc7
choncey <nchoncey@gmail.com>
2024-01-12 11:10:44 -0800

    updated ebs file

--- ebs-2dsk.tf
+++ ebs-2dsk.tf
@@ -5,13 +5,9 @@
     volume_size = 8  # Adjust the size based on your needs
   }
 
-  // Attach an additional EBS volume (2 GB)
+  // Attach an additional EBS volume (10 GB)
   ebs_block_device {
     device_name = "/dev/sdb"
-    volume_size = 2
+    volume_size = 10
     volume_type = "gp2"  # Adjust volume type as needed
-  }
-
-  tags = {
-    Name = var.name_tag,
   }
