7b5e19d136978bc1d77428f128193bd29b2d3c0e
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-12 12:49:02 -0800

    Update ssh.tf
    "vpc-02ef769587dd081cf" old

--- ssh.tf
+++ ssh.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "my_sg" {
        name        = "my_sg"
        description = "allow ssh access"
-       vpc_id      = "vpc-02ef769587dd081cf"
+       vpc_id      = "blueteam"
 
        #Not redundant - Inbound Access
        ingress {
