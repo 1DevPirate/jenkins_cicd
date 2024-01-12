af3e6bcd436a41a0c3582a9634781a28aa528d51
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-12 14:41:13 -0800

    Update sudo-permission.tf
    updated permission file

--- sudo-permission.tf
+++ sudo-permission.tf
@@ -1,15 +1,15 @@
 # Create a local user
-resource "null_resource" "choncey" {
+resource "null_resource" "create_user" {
   provisioner "local-exec" {
     command = "sudo useradd -m -s /bin/bash choncey"
   }
 }
 
 # Create a sudoers file for the user
-resource "null_resource" "choncey" {
+resource "null_resource" "create_sudoers" {
   provisioner "local-exec" {
     command = <<-EOT
-      echo 'username ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/username
+      echo 'username ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/choncey
       sudo chmod 440 /etc/sudoers.d/choncey
     EOT
   }
