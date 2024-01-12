6ffa2b6520aab3f47de87cb5ecc52cb49b6f270c
choncey <nchoncey@gmail.com>
2024-01-12 13:45:51 -0800

    updated bash scipt for apache

--- apache2-bs.tf
+++ apache2-bs.tf
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Ask the user a question
-echo "Do you want to go to jail, or do you want to go home?"
-read response
+while true; do
+  # Ask the user a question
+  echo "Do you want to go to jail, or do you want to go home?"
+  read response
 
-if [ "$response" = 'home' ] || [ "$response" = 'yes' ]; then
-  echo "Glad you want to go home."
-  echo "Aight, I got you."
+  if [ "$response" = 'home' ] || [ "$response" = 'yes' ]; then
+    echo "Glad you want to go home."
+    echo "Aight, I got you."
 
-  # Install apache2
-  sudo apt install apache2
+    # Install apache2
+    sudo apt install apache2
 
-  # Install mysql
-  sudo apt install mysql-server
+    # Install mysql
+    sudo apt install mysql-server
 
-  # Install postgres
-  sudo apt install postgresql
+    # Install postgres
+    sudo apt install postgresql
 
-  # Additional commands or actions can be added here
-else
-  echo "You made a different choice."
-  # Add actions for the alternative choice if needed
+    break  # Exit the loop if the user makes a valid choice
+  else
+    echo "Invalid response. Please enter 'home' or 'yes'."
+  
 fi
-
