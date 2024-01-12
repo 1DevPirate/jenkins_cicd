19152f8533af7e1a68496b7bd389b41b961355fc
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-12 11:28:55 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "aws_key_pair" "autodeploy" {
-#  key_name   = "autodeploy"  # Set a unique name for your key pair
+  key_name   = "autodeploy"  # Set a unique name for your key pair
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
+  key_name      = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
 
   // Other instance configurations...
 
@@ -20,4 +20,5 @@ resource "aws_instance" "public_instance" {
     device_name = "/dev/sdb"
     volume_size = 2
     volume_type = "gp2"  # Adjust volume type as needed
+  }
 }
