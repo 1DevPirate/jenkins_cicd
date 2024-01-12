5146f3f1e278445946bb4cba16f7c8362312e6b2
choncey <nchoncey@gmail.com>
2024-01-12 13:57:11 -0800

    updated script for apache, and others

--- apache2-bs.tf
+++ apache2-bs.tf
@@ -2,11 +2,11 @@
 sudo apt update
 
 # Install apache2
-sudo apt install apache2
+sudo apt install apache2 -y
 
 # Install mysql
-sudo apt install mysql-server
+sudo apt install mysql-server -y
 
 # Install postgres
-sudo apt install postgresql
+sudo apt install postgresql -y
 
