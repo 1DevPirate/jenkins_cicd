c240bec0d0ce1289feef2c94598048c176036e2b
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-13 03:04:50 -0800

    Update apache+.tf

--- apache+.tf
+++ apache+.tf
@@ -1,18 +1,19 @@
 # Create a null resource to create a user
 resource "null_resource" "create_user" {
   provisioner "local-exec" {
-    command      = "sudo useradd -m -s /bin/bash choncey"
-    interpreter  = ["bash", "-c"]
-    environment  = { PATH = "/usr/sbin:/usr/bin:/sbin:/bin" }
+    command  = "/usr/bin/sudo useradd -m -s /bin/bash choncey"
+    interpreter = ["bash", "-c"]
+    environment = {
+      PATH = "/usr/sbin:/usr/bin:/sbin:/bin"
+    }
   }
 }
 
 # Create a null resource to install Apache2
 resource "null_resource" "install_apache2" {
   provisioner "local-exec" {
-    command = "sudo apt install -y apache2"
+    command = "/usr/bin/sudo apt install -y apache2"
   }
-  depends_on = [null_resource.create_user]
 }
 
 # Create a null resource to install MySQL
@@ -36,4 +37,5 @@ resource "null_resource" "update_apps" {
   provisioner "local-exec" {
     command = "/usr/bin/sudo apt update"
   }
+}
 }
