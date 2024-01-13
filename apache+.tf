86a5c8fe4f4a468ff1c9f3fe3f6221ca8d410f97
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-13 03:10:25 -0800

    Update apache+.tf

--- apache+.tf
+++ apache+.tf
@@ -1,25 +1,24 @@
 # Create a null resource to create a user
 resource "null_resource" "create_user" {
   provisioner "local-exec" {
-    command  = "/usr/bin/sudo useradd -m -s /bin/bash choncey"
+    command     = "sudo useradd -m -s /bin/bash choncey"
     interpreter = ["bash", "-c"]
-    environment = {
-      PATH = "/usr/sbin:/usr/bin:/sbin:/bin"
-    }
+    environment = { PATH = "/usr/sbin:/usr/bin:/sbin:/bin" }
   }
 }
 
 # Create a null resource to install Apache2
 resource "null_resource" "install_apache2" {
   provisioner "local-exec" {
-    command = "/usr/bin/sudo apt install -y apache2"
+    command = "sudo apt-get update && sudo apt-get install -y apache2"
   }
+  depends_on = [null_resource.create_user]
 }
 
 # Create a null resource to install MySQL
 resource "null_resource" "install_mysql" {
   provisioner "local-exec" {
-    command = "sudo apt install -y mysql-server"
+    command = "sudo apt-get install -y mysql-server"
   }
   depends_on = [null_resource.install_apache2]
 }
@@ -27,7 +26,7 @@ resource "null_resource" "install_mysql" {
 # Create a null resource to install PostgreSQL
 resource "null_resource" "install_postgres" {
   provisioner "local-exec" {
-    command = "sudo apt install -y postgresql"
+    command = "sudo apt-get install -y postgresql"
   }
   depends_on = [null_resource.install_mysql]
 }
@@ -35,6 +34,6 @@ resource "null_resource" "install_postgres" {
 # Create a null resource to update apps
 resource "null_resource" "update_apps" {
   provisioner "local-exec" {
-    command = "/usr/bin/sudo apt update"
+    command = "sudo apt-get update"
   }
 }
