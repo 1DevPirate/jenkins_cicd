d593103bb0bc7c5f3990de14f7b3389c6747fc41
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-13 03:18:55 -0800

    Create s3bucket.tf

---
+++ s3bucket.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-s3-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "wesoblue"
+  }
+}
+
+resource "aws_s3_bucket" "pipelineartifactstore" {
+  bucket = "${var.prefix}-${var.namespace}-${var.stage}-pipeline-artifactstore"
+  acl    = "private"
+  force_destroy = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  tags = var.default_tags
+}
