13beb3554b9feed7ad4b17bf937faf261914823e
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-14 14:16:15 -0800

    Update apatest.tf

--- apatest.tf
+++ apatest.tf
@@ -1,9 +1,19 @@
 # Create a null resource to install Apache2
 resource "null_resource" "install_apache2" {
   provisioner "local-exec" {
-    command = "sudo apt install -y apache2"
+    command  = "/usr/bin/sudo apt install -y apache2"
+    interpreter = ["bash", "-c"]
   }
 }
+
+# Create a null resource to update apps
+resource "null_resource" "update_apps" {
+  provisioner "local-exec" {
+    command  = "/usr/bin/sudo apt update"
+    interpreter = ["bash", "-c"]
+  }
+}
+
 
 # Create a null resource to install MySQL
 resource "null_resource" "install_mysql" {
@@ -19,11 +29,4 @@ resource "null_resource" "install_postgres" {
     command = "sudo apt install -y postgresql"
   }
   depends_on = [null_resource.install_mysql]
-}
-
-# Create a null resource to update apps
-resource "null_resource" "update_apps" {
-  provisioner "local-exec" {
-    command = "sudo apt update"
-  }
 }
