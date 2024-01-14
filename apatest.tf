75673ec78e3923648990f79c4f626b28e0ca88cf
Choncey <147649713+1DevPirate@users.noreply.github.com>
2024-01-14 14:08:21 -0800

    Update apatest.tf

--- apatest.tf
+++ apatest.tf
@@ -1,10 +1,6 @@
 # Create a null resource to create a user
 resource "null_resource" "create_user" {
-  provisioner "local-exec" {
-    command      = "useradd -m -s /bin/bash choncey"
-    interpreter  = ["bash", "-c"]
-    environment  = { PATH = "/usr/sbin:/usr/bin:/sbin:/bin" }
-  }
+  # Omitted the local-exec provisioner for user creation
 }
 
 # Create a null resource to install Apache2
@@ -12,7 +8,6 @@ resource "null_resource" "install_apache2" {
   provisioner "local-exec" {
     command = "apt install -y apache2"
   }
-  depends_on = [null_resource.create_user]
 }
 
 # Create a null resource to install MySQL
@@ -31,9 +26,4 @@ resource "null_resource" "install_postgres" {
   depends_on = [null_resource.install_mysql]
 }
 
-# Create a null resource to update apps
-resource "null_resource" "update_apps" {
-  provisioner "local-exec" {
-    command = "apt update"
-  }
-}
+# Omitted the null resource for updating apps
